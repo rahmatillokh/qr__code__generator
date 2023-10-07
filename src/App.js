b6a99320153aa50e204e40fb723029be9b2e4c80
rahmtillokh <webcoder292@gmail.com>
2023-10-07 17:30:51 +0500

    fixed

--- src/App.js
+++ src/App.js
@@ -12,27 +12,44 @@ function App() {
     setLoading(true);
     axios
       .get(`http://api.qrserver.com/v1/create-qr-code/?data=${value}`, {
-        responseType: "blob", // Specify the response type as Blob
+        responseType: "blob",
       })
       .then((response) => {
-        const blob = new Blob([response.data]); // Create a Blob from the response data
-        const url = window.URL.createObjectURL(blob);
-        setUrl(url);
-        setData(url); // Set data to the Blob URL
+        const blob = new Blob([response.data]);
+        const qrCodeUrl = window.URL.createObjectURL(blob);
+        setUrl(qrCodeUrl);
+        setData(qrCodeUrl);
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
-        setData(false); // Set data to false if there's an error
+        setData(false);
         setLoading(false);
       });
   };
 
   useEffect(() => {
     if (data) {
-      handleFetch(); // Automatically fetch when data changes
+      handleFetch();
     }
   }, [data]);
+
+  const handleShare = () => {
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "QR Code",
+          text: "Check out this QR Code!",
+          url: url, // Share the URL of the generated QR code
+        })
+        .then(() => {
+          console.log("Shared successfully");
+        })
+        .catch((error) => {
+          console.error("Share failed:", error);
+        });
+    }
+  };
 
   return (
     <div className="header">
@@ -50,9 +67,12 @@ function App() {
         </button>
 
         {data ? (
-          <a href={url} download={Math.random() * 10000000000 + ".png"}>
-            Yuklab olish
-          </a>
+          <div>
+            <a href={url} download={Math.random() * 10000000000 + ".png"}>
+              Yuklab olish
+            </a>
+            <button onClick={handleShare}>Share</button>
+          </div>
         ) : null}
       </div>
     </div>
